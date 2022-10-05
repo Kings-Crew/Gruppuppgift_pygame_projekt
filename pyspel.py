f5244e28298778d48e2f4c62438aa8705a015236
jonghi11 <jonatan.ghirmay@gmail.com>
2022-10-05 13:21:10 +0200

    andra commit för blocks fungerar

--- pyspel.py
+++ pyspel.py
@@ -1,66 +1,97 @@
-from email.mime import image
 import pygame
-import time
+from pygame.locals import *
 
+pygame.init()
 
-pygame.init() # 
+screen_width = 600
+screen_height = 600
 
- 
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
- 
-
-
-screen_width = 1300 # Detta är skärmstorleken i högd och bredd
-screen_height = 1300
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Breakout')
 
-
-
-pygame.display.update() # Sätter rubriken på fönstret(Dashlak)
-pygame.display.set_caption("Dashlak")
-
-#define block colours
+#define colours
 bg = (234, 218, 184)
 #block colours
 block_red = (242, 85, 96)
 block_green = (86, 174, 87)
 block_blue = (69, 177, 232)
 
+
 #define game variables
 cols = 6
 rows = 6
+
+
 
 #brick wall class
 class wall():
     def __init__(self):
         self.width = screen_width // cols
         self.height = 50
-           
-   
 
-        
+    def create_wall(self):
+        self.blocks = []
+        #define an empty list for an individual block
+        block_individual = []
+        for row in range(rows):
+            #reset the block row list
+            block_row = []
+            #iterate through each column in that row
+            for col in range(cols):
+                #generate x and y positions for each block and create a rectangle from that
+                block_x = col * self.width
+                block_y = row * self.height
+                rect = pygame.Rect(block_x, block_y, self.width, self.height)
+                #assign block strength based on row
+                if row < 2:
+                    strength = 3
+                elif row < 4:
+                    strength = 2
+                elif row < 6:
+                    strength = 1
+                #create a list at this point to store the rect and colour data
+                block_individual = [rect, strength]
+                #append that individual block to the block row
+                block_row.append(block_individual)
+            #append the row to the full list of blocks
+            self.blocks.append(block_row)
 
 
-done = False
-x = 5
-y = 5
-while not done:
+    def draw_wall(self):
+        for row in self.blocks:
+            for block in row:
+                #assign a colour based on block strength
+                if block[1] == 3:
+                    block_col = block_blue
+                elif block[1] == 2:
+                    block_col = block_green
+                elif block[1] == 1:
+                    block_col = block_red
+                pygame.draw.rect(screen, block_col, block[0])
+                pygame.draw.rect(screen, bg, (block[0]), 2)
+
+
+
+#create a wall
+wall = wall()
+wall.create_wall()
+
+
+
+run = True
+while run:
+
+    screen.fill(bg)
+
+    #draw wall
+    wall.draw_wall()
+
+
     for event in pygame.event.get():
-        if event.type == pygame:
-            done = True
-    pygame.draw.rect(screen, (0,128,255), pygame.Rect(x, y, 10, 10))
+        if event.type == pygame.QUIT:
+            run = False
 
 
+    pygame.display.update()
 
-    pressed = pygame.key.get_pressed() # Sätter användning av pilarna för rörelse.
-    if pressed[pygame.K_UP]: y -= 5
-    if pressed[pygame.K_DOWN]: y+= 5
-    if pressed[pygame.K_RIGHT]: x += 5
-    if pressed[pygame.K_LEFT]: x -= 5
-
-
-
-
-    pygame.display.flip()
+pygame.quit()
