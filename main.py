3d11c2dd4d0d291552e0eea00bc8f4b6b68bd860
Shallange <jimmy.kroneld@hotmail.com>
2022-10-07 13:29:39 +0200

    lade till main filen för spelet

--- main.py
+++ main.py
@@ -1,47 +1,99 @@
 import pygame
 import time
 import os
-#from pygame.locals import *
+from random import randint as RI
+
 pygame.init()
-FPS = 30
-WIDTH, HEIGHT = 1300, 1300
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-GREY = (90, 90, 90)
-
+collision_threshhold = 12
+max_ball_speed = 5 
+WHITE = (255,255,255)
+FPS = 60
+WIDTH, HEIGHT = 1280 , 720
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()#a group that holds all sprites,
+#img_folder = os.path.join('assets\images')#path to image folder, saved as a variabel
 
+def draw_window():#fills the screen with color white and updates the screen 
+    screen.fill((WHITE))
 
-def draw_window():  # fills the screen with color white and updates the screen
-    screen.fill((GREY))
-
-
-class board(object):
+class board(pygame.sprite.Sprite):
     def __init__(self):
-        self.image = pygame.image.load(os.path.join(
-            'assets/images/menu_assets\images', 'breakout_piece_blue.png'))
-        self.y = 560
-        self.x = 350
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('assets','breakout_piece_blue.png')).convert_alpha()
+        self.rect = self.image.get_rect()#creates a rect around image
+        self.y_pos = HEIGHT -100
+        self.x_pos = WIDTH /2
+        self.rect.center = (self.x_pos, self.y_pos)
+        self.direction = 0
+        self.speed = 10 
+    def update(self):
+        if self.x_pos < 32:#32 is halv of board length in pixels
+            self.x_pos = 32
+        if self.x_pos > WIDTH -32:
+            self.x_pos = WIDTH -32
+        self.rect.center = (self.x_pos,self.y_pos)
+    def move_left(self):
+        self.x_pos -= self.speed
+        self.direction = -1
+    def move_right(self):
+        self.x_pos += self.speed
+        self.direction = 1
+    
+        
+class ball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.x_pos = WIDTH / 2
+        self.y_pos = HEIGHT / 2
+        self.image = pygame.image.load(os.path.join('assets','ball_piece_blue.png')).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (12,12))
+        self.rect = self.image.get_rect()
+        self.velocity = [RI(4,5), RI(4,5)]#self.velocity[0] is the first random for x pos and self.velocity[1] is the other random for y 
+        self.rect.center = (self.x_pos, self.y_pos)
+    def update(self):
+        # 6 is halv of the ball's size in pixels 
+        if self.x_pos < 6 or self.x_pos > WIDTH - 6: #if ball collide with left or rigth side it will reverse 
+            self.velocity[0] = -self.velocity[0]
+        if self.y_pos < 6: #if the ball collide with the top of the screen set the velocity to negative
+            self.velocity[1] = -self.velocity[1]
+        if self.velocity[0] == 0:#if the velociy reach 0 in either x or y direction, add velocity 
+            self.velocity[0] += 1
+        if self.velocity[1] == 0:
+            self.velocity[1] += 1
+        #sets the velocity to the current position so de ball will move acordingly 
+        self.x_pos += self.velocity[0] 
+        self.y_pos += self.velocity[1]
+        self.rect.center = (self.x_pos, self.y_pos)
+    def reset(self):# when the bal resets it will spawn in the center of the screen and have random direction(velocity)
+        self.velocity = [RI(4,5), RI(4,5)]
+        self.x_pos = WIDTH / 2
+        self.y_pos = HEIGHT / 2
+        self.rect.center = (self.x_pos, self.y_pos) 
 
-    def handle_keys(self):
-        key = pygame.key.get_pressed()
-        dist = 10
-        """ if key[pygame.K_DOWN]:
-            self.y += dist
-        elif key[pygame.K_UP]:
-            self.y -= dist """
-        if key[pygame.K_RIGHT]:
-            self.x += dist
-        elif key[pygame.K_LEFT]:
-            self.x -= dist
+    def check_board_collision(self,board):
+         if self.rect.colliderect(board.rect):
+            if abs(self.rect.bottom - board.rect.top) < collision_threshhold and self.velocity[1]>0:
+                self.velocity[1] = -self.velocity[1]
+                self.velocity[0] += board.direction
+                if self.velocity[0] > max_ball_speed:
+                    self.velocity[0] = max_ball_speed
+                if self.velocity[0] < -max_ball_speed:
+                    self.velocity[0] = -max_ball_speed
+                else:
+                    self.velocity[0] *= -1
+            
+    def is_out_bounds(self):
+        return self.y_pos > HEIGHT
 
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
 
-
-""" class player:
-    pass """
+class player:
+    pass
 
 board = board()
+ball = ball()
+all_sprites.add(board,ball)
+    
 
 
 class Main:
@@ -54,11 +106,20 @@ class Main:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
-            board.handle_keys()
-            draw_window()
-            board.draw(screen)
-            pygame.display.update()
-
+            
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                board.move_left()
+            if key[pygame.K_RIGHT]:
+                board.move_right()            
+            
+            ball.check_board_collision(board)
+            if ball.is_out_bounds():
+                ball.reset()
+            all_sprites.update()#uppdates all sprites that is inside sprite.group()
+            draw_window()#makes the bakground white
+            all_sprites.draw(screen)#prints/drwas out all sprites in sprite.group()
+            pygame.display.update()       
+          
     if __name__ == "__main__":
         main()
