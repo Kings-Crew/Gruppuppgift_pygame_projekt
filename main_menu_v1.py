25e6069adfebbe77bb0e80ec5d9e59033404ffd6
Marcus <marcus@Marcuss-MacBook-Air.local>
2022-10-13 12:39:23 +0200

    Ändrade färgen på textknapparna i startmenyn till svart

--- main_menu_v1.py
+++ main_menu_v1.py
@@ -89,15 +89,15 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#0a0d0a")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
-                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                             text_input="PLAY", font=get_font(75), base_color="#0a0d0a", hovering_color="White")
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
-                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                                text_input="OPTIONS", font=get_font(75), base_color="#0a0d0a", hovering_color="White")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
-                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                             text_input="QUIT", font=get_font(75), base_color="#0a0d0a", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
